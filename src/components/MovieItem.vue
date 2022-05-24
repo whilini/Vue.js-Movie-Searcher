2761cba729fa3d9e0038a839ca122b22a4829b06
whilini <whi0306@naver.com>
2022-05-25 03:55:39 +0900

    feat: movie list & movie item & loading circle

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>{{ movie.Title }}</div>
+  <div 
+    class="movie"
+    :style="{ backgroundImage: `url(${movie.Poster})`}">
+    <div class="info">
+      <div class="year">
+        {{ movie.Year }}
+      </div>
+      <div class="title">
+        {{ movie.Title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,3 +23,51 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.movie {
+  $width: 168px;
+  width: $width;
+  height: $width * 3 / 2;
+  margin: 10px;
+  border-radius: 4px;
+  background-color: $gray-400;
+  background-size: cover;
+  overflow: hidden;
+  position: relative;
+  &:hover::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      border: 6px solid $primary;
+    }
+  .info {
+    background-color: rgba($black, .3);
+    width: 100%;
+    padding: 14px;
+    font-size: 14px;
+    text-align: center;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    color:#fff;
+    transition: all .3s;
+    backdrop-filter: blur(10px);
+    .year {
+      color: $primary
+    }
+    .title{
+      color: $white;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis
+    }
+  }
+}
+</style>
--- src/store/moive.js
+++ src/store/moive.js
@@ -7,7 +7,7 @@ export default {
   // data
   state: () => ({
     movies: [],
-    message:'',
+    message:'Search for the movie title!',
     loading: false
   }),
   // computed
@@ -28,28 +28,70 @@ export default {
   // 비동기
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const { title, type, number, year} = payload
-      const OMDB_API_KEY = '7035c60c'
+      // searchMovies 여러번 실행 방지
+      // loading이 true이면 아래 함수를 실행하지 않고 종료
+      if (state.loading) return
 
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
-      const { Search, totalResults } = res.data
-      commit('updateState', {
-        movies: _uniqBy(Search, 'imdbID'),
+      // loading 이전 movie list 초기화(기본 message 삭제)
+      commit('updateState',{
+        message: '',
+        loading: true
       })
-      const total = parseInt(totalResults, 10)
-      const pageLength = Math.ceil(total / 10)
-
-      // 추가 요청
-      if(pageLength > 1) {
-        for (let page = 2; page <= pageLength; page += 1){
-          if (page > (number / 10)) break
-          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [...state.movies, ..._uniqBy(Search, 'imdbID')]
-          })
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID'),
+        })
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
+  
+        // 추가 요청
+        if(pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1){
+            if (page > (payload.number / 10)) break
+            const res = await _fetchMovie({
+              ...payload,
+              page
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [...state.movies, ..._uniqBy(Search, 'imdbID')]
+            })
+          }
         }
+      } catch(message) {
+        commit('updateState', {
+          movies: [],
+          message: message
+        })
+      } finally {
+        commit('updateState',  {
+          loading: false
+        })
       }
     }
   }
+}
+
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload
+  const OMDB_API_KEY = '7035c60c'
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then((res) => {
+        if (res.data.Error) {
+          reject(res.data.Error)
+        }
+        resolve(res)
+      })
+      .catch((err) => {
+        reject(err.message)
+      })
+  })
 }
