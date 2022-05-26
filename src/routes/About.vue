1aab25e468a0fff89d81750b8935f85bd356bdde
whilini <whi0306@naver.com>
2022-05-26 15:06:43 +0900

    feat: about page & upload about picture

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,79 @@
 <template>
-  <h1>About!</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader
+        v-if="imageLoading"
+        absolute />
+      <img
+        :src="image"
+        alt="name" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+  </div>
 </template>
+
+
+<script>
+// import buttercup from "@/assets/buttercup2.png"
+import Loader from '../components/Loader.vue'
+
+export default {
+  components: { Loader },
+  data() {
+    return {
+      imageLoading: true,
+      // buttercup: buttercup
+    }
+  },
+  computed: {
+    image() {
+      return this.$store.state.about.image
+    },
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    blog() {
+      return this.$store.state.about.blog
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+.about {
+  text-align: center;
+  .photo {
+    position:relative;
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    .img {
+      width: 100%;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "RYN",
+    email: "whiliniong@gmail.com",
+    blog: "https://github.com/whilini/Vue3-Movie-Searcher",
+    image: "/assets/buttercup2.png"
+  })
+}
